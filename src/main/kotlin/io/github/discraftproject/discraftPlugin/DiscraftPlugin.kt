39b8b02bba29996bd9e248febde211980f5f2fb6
gudtldn <gudtldn1234@gmail.com>
2024-12-18 01:18:55 +0900

    🎉 init project

---
+++ src/main/kotlin/io/github/discraftproject/discraftPlugin/DiscraftPlugin.kt
@@ -0,0 +1,14 @@
+package io.github.discraftproject.discraftPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DiscraftPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
